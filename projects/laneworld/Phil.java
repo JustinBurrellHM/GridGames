07e5df43b7bdaa438d4041c620f62dd318b21458
Lucca Correia <luccacorreia@Luccas-MacBook-Pro-2.local>
2022-02-18 08:43:04 -0500

    phil

---
+++ projects/laneworld/Phil.java
@@ -0,0 +1,16 @@
+/*
+Default: 4/8 
+Ability: Economics - 
+*/
+
+public class Phil extends Pet {
+    public Phil(int id, int direction, Team team) {
+        super(id, "Phil", 4, 8, direction, team);
+                                                  }
+
+
+
+
+
+
+ }
